3b818ca0567a7f7b51480f01233de44eeb6f4ae1
Victor Poughon <victor.poughon@gmail.com>
2019-08-15 08:59:17 +0200

    Init

---
+++ DingPics.lua
@@ -0,0 +1,8 @@
+local frame = CreateFrame("Frame")
+frame:Hide()
+frame:RegisterEvent("PLAYER_LEVEL_UP")
+frame:SetScript("OnEvent", function (self, event)
+	-- wait a bit so the yellow animation appears 300ms seems good
+	C_Timer.After(.3, Screenshot)
+end
+)
--- MacroDatabase/rogue.lua
+++
@@ -1,7 +0,0 @@
-if macro_database == nil then
-    macro_database = {}
-end
-
-macro_database.rogue = {
-
-}
