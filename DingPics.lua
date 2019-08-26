193fa2b4634c149000362271cb0d58b260eb0594
Steven <texruska@users.noreply.github.com>
2019-08-26 13:48:20 +0100

    Add /played to screenshot

--- DingPics.lua
+++ DingPics.lua
@@ -3,6 +3,7 @@ frame:Hide()
 frame:RegisterEvent("PLAYER_LEVEL_UP")
 frame:SetScript("OnEvent", function (self, event)
 	-- wait a bit so the yellow animation appears 300ms seems good
+	RequestTimePlayed()
 	C_Timer.After(.3, Screenshot)
 end
 )
