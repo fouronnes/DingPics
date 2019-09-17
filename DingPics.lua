def3bd6b1312400a734ce38e8645e5e2717fcc17
Isaac Simmons <47719635+IsaacSimmons96@users.noreply.github.com>
2019-09-17 22:44:03 +0100

    Changed to 400ms
    Changed to 400ms as it wasnt catching the yellow animation.

--- DingPics.lua
+++ DingPics.lua
@@ -3,6 +3,6 @@ frame:Hide()
 frame:RegisterEvent("PLAYER_LEVEL_UP")
 frame:SetScript("OnEvent", function (self, event)
 	-- wait a bit so the yellow animation appears 300ms seems good
-	C_Timer.After(.3, Screenshot)
+	C_Timer.After(.4, Screenshot)
 end
 )
