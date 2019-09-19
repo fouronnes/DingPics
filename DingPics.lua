7b26e36d036aac8f10784bed31544edbfa6b948f
Peter Wedder <burneddi@gmail.com>
2019-09-20 01:12:28 +0300

    Update DingPics.lua

--- DingPics.lua
+++ DingPics.lua
@@ -2,7 +2,8 @@ local frame = CreateFrame("Frame")
 frame:Hide()
 frame:RegisterEvent("PLAYER_LEVEL_UP")
 frame:SetScript("OnEvent", function (self, event)
-	-- wait a bit so the yellow animation appears 300ms seems good
-	C_Timer.After(.3, Screenshot)
+	-- wait a bit so the yellow animation appears
+	RequestTimePlayed()
+	C_Timer.After(1, Screenshot)
 end
 )
