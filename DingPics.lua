d0cf59b6143caa49e73552ed78a02462ff06b8b8
Mark Hewitt <m@mh1.co>
2022-07-26 13:21:37 -0400

    feat: GitHub auto-release and updated TOC file

--- DingPics.lua
+++ DingPics.lua
@@ -4,6 +4,6 @@ frame:RegisterEvent("PLAYER_LEVEL_UP")
 frame:SetScript("OnEvent", function (self, event)
 	-- wait a bit so the yellow animation appears 300ms seems good
 	RequestTimePlayed()
-	C_Timer.After(.3, Screenshot)
+	C_Timer.After(1, Screenshot)
 end
 )
