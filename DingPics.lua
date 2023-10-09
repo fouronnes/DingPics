bbec985f56fa01dc9deeba2764102817f34b4516
Mark Hewitt <m@mh1.co>
2023-10-09 14:53:54 -0400

    fix: updated for 1.14.4, added death screenshot functionality

--- DingPics.lua
+++ DingPics.lua
@@ -1,9 +1,15 @@
 local frame = CreateFrame("Frame")
 frame:Hide()
+
 frame:RegisterEvent("PLAYER_LEVEL_UP")
+frame:RegisterEvent("PLAYER_DEAD")
+
 frame:SetScript("OnEvent", function (self, event)
 	-- wait a bit so the yellow animation appears 800ms seems good
 	RequestTimePlayed()
-	C_Timer.After(0.8, Screenshot)
+	C_Timer.After(0.8, function()
+		Screenshot()
+		end
+	)
 end
 )
